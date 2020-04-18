14c7a8ad159b47f07050c4b9462f26d1711489d0
Weyert de Boer <weyert@innerfuse.biz>
2020-04-18 18:25:28 +0100

    fix: removed the migration and updated the `up.aql` initial migration instead

--- migrations/1_init/up.sql
+++ migrations/1_init/up.sql
@@ -42,7 +42,7 @@ CREATE TABLE auth.accounts (
     mfa_enabled boolean DEFAULT false NOT NULL,
     ticket uuid DEFAULT public.gen_random_uuid() NOT NULL,
     ticket_expires_at timestamp with time zone DEFAULT now() NOT NULL,
-    CONSTRAINT proper_email CHECK ((email OPERATOR(public.~*) '^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+[.][A-Za-z]+$'::public.citext))
+    CONSTRAINT proper_email CHECK ((email OPERATOR(public.~*) '^[A-Za-z0-9+._%-]+@[A-Za-z0-9.-]+[.][A-Za-z]+$'::public.citext))
 );
 CREATE TABLE auth.providers (
     provider text NOT NULL
